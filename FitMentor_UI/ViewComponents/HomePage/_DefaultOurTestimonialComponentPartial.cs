105854ac30810cfe47f112347ef22fadd7a0ca6b
Öykü Ünsay <67629217+oykunsay@users.noreply.github.com>
2024-08-20 13:50:31 +0300

    Initial commit

---
+++ FitMentor_UI/ViewComponents/HomePage/_DefaultOurTestimonialComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitMentor_UI.ViewComponents.HomePage
+{
+    public class _DefaultOurTestimonialComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
